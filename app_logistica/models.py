b49ee89ba353251bc30c234608d0a66e6d4f5f86
Leonard Quispe <leonardquispe92@gmail.com>
2026-02-11 09:01:40 -0500

    Actualizacion de modelos items

--- app_logistica/models.py
+++ app_logistica/models.py
@@ -98,7 +98,7 @@ class Items(models.Model):
     nombre_item = models.CharField(max_length=100)
     descripcion_item = models.CharField(max_length=200,blank=True,null=True)
     imagen_qr = models.ImageField(upload_to='imagenes_qr/',blank=True,null=True)
-    #tipo_item = models.ForeignKey(TipoItems,on_delete=models.CASCADE,default=1)
+    tipo_item = models.ForeignKey(TipoItems,on_delete=models.CASCADE,null=True,blank=True)
     cantidad_items = models.IntegerField(default=1)
     id_area = models.ForeignKey(AreasEmpresa,on_delete=models.CASCADE,null=True,blank=True)
     id_estado = models.ForeignKey(TipoEstadoItems,on_delete=models.CASCADE)
@@ -155,11 +155,11 @@ class TiposMovimiento(models.Model):
     
 class ItemsMovimientos(models.Model):
     id_movimiento = models.AutoField(primary_key=True)
-    #tipo_movimiento = models.ForeignKey(TiposMovimiento,on_delete=models.CASCADE,default=1)
+    tipo_movimiento = models.ForeignKey(TiposMovimiento,on_delete=models.CASCADE,null=True,blank=True)#
     id_item = models.ForeignKey(Items,on_delete=models.CASCADE)
     nombre_origen = models.CharField(max_length=150)
     nombre_destino = models.CharField(max_length=150)
-    id_movimiento_referencia = models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True)
+    id_movimiento_referencia = models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True)#
     observaciones = models.CharField(max_length=200)    
     fecha_modificacion = models.DateTimeField(auto_now=True)
     
