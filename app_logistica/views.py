3f0a7f2f67b4b38269d33351a285776840af6066
Leonard Quispe <leonardquispe92@gmail.com>
2026-02-17 10:22:24 -0500

    Editar Formulario items1

--- app_logistica/views.py
+++ app_logistica/views.py
@@ -62,7 +62,7 @@ def editar_item(request,pk):
         return redirect('logistica_items')
     else:
         form = ItemsForm(instance=item)
-    return render(request,'logistica/formulario_editar_item.html',{'form':form,'item':item,'inventario':inventario'})
+    return render(request,'logistica/formulario_editar_item.html',{'form':form,'item':item,'inventario':inventario})
     
 @login_required(login_url='login_logistica')    
 def inventariar_articulo(request,pk):
