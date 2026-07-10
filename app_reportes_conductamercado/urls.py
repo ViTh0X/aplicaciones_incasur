563e7f077ed4ca3dc109787539b2cf191275bba2
Leonard Quispe Cardenas <leonardquispe92@gmail.com>
2026-07-10 13:26:29 -0500

    Añadiendo reporte comisiones gastos

--- app_reportes_conductamercado/urls.py
+++ app_reportes_conductamercado/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('comisiones-gastos',views.comisiones_gastos,name='comisiones_gastos'),
+    path('reporte-rr3',views.reporte_rr3,name='reporte_rr3'),
 ]
--- app_reportes_conductamercado/views.py
+++ app_reportes_conductamercado/views.py
@@ -43,3 +43,34 @@ def comisiones_gastos(request):
     else:
         form = FechasComisionesGastosForm()
     return render(request, 'reportes_conductamercado/comisiones_gastos.html', {'form': form})
+
+def reporte_rr3(request):
+    if request.method == 'POST':
+        form = FechasComisionesGastosForm(request.POST)
+        if form.is_valid():
+            fecha_inicio = form.cleaned_data['fecha_inicio']
+            fecha_fin = form.cleaned_data['fecha_fin']
+            query = "SELECT * FROM fxBerenise(%s, %s);"
+            with connections['dat-cierre'].cursor() as cursor:
+                cursor.execute(query, [fecha_inicio, fecha_fin])
+                data = cursor.fetchall()
+                
+                # Obtenemos los nombres de las columnas para que el DataFrame no quede solo con números
+                columnas = [desc[0] for desc in cursor.description]
+                
+                # Creamos el DataFrame
+                df = pd.DataFrame(data, columns=columnas)            
+                df = df.rename(columns={
+                    'ccodigo': 'Codigo',
+                    'cdescri': 'Operaciones-Servicios-Productos',
+                    'ccanal': 'Canal',
+                    'cpernat': 'PersonaNatural', 
+                    'cperjur': 'PersonaJuridica',                    
+                })
+                response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                response['Content-Disposition'] = f'attachment; filename="ReporteRR3de{fecha_inicio}-{fecha_fin}.xlsx"'
+                df.to_excel(response,index=False,sheet_name='InventarioHardware')
+                return response 
+    else:
+        form = FechasComisionesGastosForm()
+    return render(request, 'reportes_conductamercado/reporte_rr3.html', {'form': form})
