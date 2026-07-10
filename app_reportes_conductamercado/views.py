a2219360d7f89bcd24cd86f1d270379e54968e1a
Leonard Quispe Cardenas <leonardquispe92@gmail.com>
2026-07-10 12:58:34 -0500

    Añadiendo reporte comisiones gastos

--- app_reportes_conductamercado/views.py
+++ app_reportes_conductamercado/views.py
@@ -10,28 +10,36 @@ def comisiones_gastos(request):
         form = FechasComisionesGastosForm(request.POST)
         if form.is_valid():
             fecha_inicio = form.cleaned_data['fecha_inicio']
-            fecha_fin = form.cleaned_data['fecha_fin']        
+            fecha_fin = form.cleaned_data['fecha_fin']
             query = "SELECT * FROM fxcomision_gastos(%s, %s);"
-            df = pd.read_sql_query(query, connections['dat-cierre'], params=[fecha_inicio, fecha_fin])
-            df = df.rename(columns={
-                'cproducto': 'Cod.',
-                'tipoproducto': 'TIPO PROUCTO',
-                'ccategoria': 'Cod',
-                'categoriaconcepto': 'CATEGORIA O CONCEPTO',
-                'cdenomincacion': 'Cod',
-                'denominacion': 'DENOMINACION',
-                'moneda': 'MONEDA',
-                'periodicidad': 'PERIODICIDAD',
-                'tipocomisiongasto': 'TIPO COMISION o GASTO',
-                'porcenmin': 'PORCENTAJE MINIMO',
-                'porcenmax': 'PORCENTAJE MAXIMO',
-                'montomin': 'MONTO MINIMO',
-                'montomax': 'MONTO MAXIMO',
-            })
-            response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            response['Content-Disposition'] = f'attachment; filename="ComisionesGastosde{fecha_inicio}-{fecha_fin}.xlsx"'
-            df.to_excel(response,index=False,sheet_name='InventarioHardware')
-            return response 
+            with connections['dat-cierre'].cursor() as cursor:
+                cursor.execute(query, [fecha_inicio, fecha_fin])
+                data = cursor.fetchall()
+                
+                # Obtenemos los nombres de las columnas para que el DataFrame no quede solo con números
+                columnas = [desc[0] for desc in cursor.description]
+                
+                # Creamos el DataFrame
+                df = pd.DataFrame(data, columns=columnas)            
+                df = df.rename(columns={
+                    'cproducto': 'Cod.',
+                    'tipoproducto': 'TIPO PROUCTO',
+                    'ccategoria': 'Cod',
+                    'categoriaconcepto': 'CATEGORIA O CONCEPTO',
+                    'cdenomincacion': 'Cod',
+                    'denominacion': 'DENOMINACION',
+                    'moneda': 'MONEDA',
+                    'periodicidad': 'PERIODICIDAD',
+                    'tipocomisiongasto': 'TIPO COMISION o GASTO',
+                    'porcenmin': 'PORCENTAJE MINIMO',
+                    'porcenmax': 'PORCENTAJE MAXIMO',
+                    'montomin': 'MONTO MINIMO',
+                    'montomax': 'MONTO MAXIMO',
+                })
+                response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                response['Content-Disposition'] = f'attachment; filename="ComisionesGastosde{fecha_inicio}-{fecha_fin}.xlsx"'
+                df.to_excel(response,index=False,sheet_name='InventarioHardware')
+                return response 
     else:
         form = FechasComisionesGastosForm()
     return render(request, 'reportes_conductamercado/comisiones_gastos.html', {'form': form})
